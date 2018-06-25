bb9ca7cb2b1bc45dd43443a85e855c21ae648967
lewisc2303 <lewiscoleman1995@hotmail.com>
2018-06-25 17:54:42 +0100

    Changing of tie and win code into repeatable functions outside of while loop adn simplify tie function

--- Tic_Tac_Toe_Game.py/Tic_Tac_Toe_Game.py/Tic_Tac_Toe_Game.py.py
+++ Tic_Tac_Toe_Game.py/Tic_Tac_Toe_Game.py/Tic_Tac_Toe_Game.py.py
@@ -1,9 +1,9 @@
-
+#Tic Tac Toe game against the computer for HolidayCheck
 import os
 import random 
 import time
 
-#Creation steps
+#Instructions
 #Create game board
 #User input
 #Winning capability
@@ -13,7 +13,7 @@ import time
 #Initialise board
 board = ["", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-#Draw the board to contatain the above list
+#Drawing of the board to contain the board list
 def draw_board():
     print("   |   |  ")
     print(" " + board[1] + " | " + board[2] + " | " + board[3] + " ")
@@ -27,30 +27,44 @@ def draw_board():
     print(" " + board[7] + " | " + board[8] + " | " + board[9] + " ")
     print("   |   |  ")
 
-#Inputing number which correlates to board and represented by an X
+#Function to determine winner
+def is_winner(board, player):
+    if (board[1]== player and board[2] == player  and board[3] == player ) or \
+     (board[4]== player and board[5] == player  and board[6] == player ) or \
+     (board[7]== player and board[8] == player  and board[9] == player ) or \
+     (board[1]== player and board[4] == player  and board[7] == player ) or \
+     (board[2]== player and board[5] == player  and board[8] == player ) or \
+     (board[3]== player and board[6] == player  and board[9] == player ) or \
+     (board[1]== player and board[5] == player  and board[9] == player ) or \
+     (board[3]== player and board[5] == player  and board[7] == player ):   
+        return True
+    else:
+        return False
+
+#Function to determine Tie
+def is_board_full(board):
+    if " " in board:
+        return False
+    else: 
+        return True
+
+#Game loop
 while True:
     os.system('cls')
     draw_board()
     
-#user input of 'cross' at desired gridspace
+    #user input of 'cross' at desired gridspace
     user_input = input("Please select which grid space you would like to place the x?  ")
     user_input = int(user_input)
 
-    #Not enable space to be taken twice
+    #No double space useage
     if board[user_input]== " ":
         board[user_input]= "x"
     else: 
         print("This space is already taken, please choose another available space")
         time.sleep(1)
-
-    if (board[1]== "x"and board[2] == "x" and board[3] == "x") or \
-     (board[4]== "x"and board[5] == "x" and board[6] == "x") or \
-     (board[7]== "x"and board[8] == "x" and board[9] == "x") or \
-     (board[1]== "x"and board[4] == "x" and board[7] == "x") or \
-     (board[2]== "x"and board[5] == "x" and board[8] == "x") or \
-     (board[3]== "x"and board[6] == "x" and board[9] == "x") or \
-     (board[1]== "x"and board[5] == "x" and board[9] == "x") or \
-     (board[3]== "x"and board[5] == "x" and board[7] == "x"):   
+    #Check x win
+    if is_winner(board, "x"):   
         os.system("cls")
         draw_board()
         print("Congratulations you are the winner!")
@@ -59,52 +73,33 @@ while True:
     os.system('cls')
     draw_board()
     
-    #No Tie Scenario
-    #Board is full doesent work. False is flagged even when grid is full?
-    board_full = True
-    for index in range(1, 10):
-        if board[index] == " ":
-            board_full = False
-            break
     #Tie scenario        
-    if board_full == True:
+    if is_board_full(board) == True:
         os.system("cls")
         draw_board()
         print("It's a Tie!")
         break
 
-#computer input of 'circle' at desired gridspace
+    #computer input of 'circle' at desired gridspace
     computer_input = input("Please select which grid space you would like to place the o?  ")
     computer_input = int(computer_input)
 
-    #Not enable space to be taken twice
+    #No double space usage
     if board[computer_input]== " ":
         board[computer_input]= "o"
     else: 
         print("This space is already taken, please choose another available space")
         time.sleep(1)
 
-    if (board[1]== "o"and board[2] == "o" and board[3] == "o") or \
-     (board[4]== "o"and board[5] == "o" and board[6] == "o") or \
-     (board[7]== "o"and board[8] == "o" and board[9] == "o") or \
-     (board[1]== "o"and board[4] == "o" and board[7] == "o") or \
-     (board[2]== "o"and board[5] == "o" and board[8] == "o") or \
-     (board[3]== "o"and board[6] == "o" and board[9] == "o") or \
-     (board[1]== "o"and board[5] == "o" and board[9] == "o") or \
-     (board[3]== "o"and board[5] == "o" and board[7] == "o"):   
+    #check o win
+    if is_winner(board,"o"):   
         os.system("cls")
         draw_board()
         print("The computer won. The machines are taking over...")
         break
-    #No Tie Scenario
-    #Board is full doesent work. False is flagged even when grid is full?
-    board_full = True
-    for index in range(1, 10):
-        if board[index] == " ":
-            board_full = False
-            break
+
     #Tie scenario        
-    if board_full == True:
+    if is_board_full(board) == True:
         os.system("cls")
         draw_board()
         print("It's a Tie!")
